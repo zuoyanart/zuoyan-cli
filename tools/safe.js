204daa979321a8c1aa26d4e4109da151654e5cab
左盐 <huabinglan@163.com>
2019-10-28 18:02:13 +0800

    fixbug

--- tools/safe.js
+++ tools/safe.js
@@ -1,38 +1,46 @@
 const shell = require('shelljs');
 const chalk = require('chalk');
 const inquirer = require('inquirer');
-const Nunjucks = require('./nunjucks');
 const fs = require('fs-extra');
 const path = require('path');
 const logSymbols = require('log-symbols');
 const os = require('os');
+const bytenode = require('bytenode');
+const JavaScriptObfuscator = require('javascript-obfuscator');
 
 module.exports = class {
+  /**
+   * thinkjs 项目加密
+   */
   thinkjs() {
     // 更改think-loader方法
     const exePath = process.cwd();
-    // console.log(path.resolve(__dirname, '../node_modules/bytenode/cli.js').toString() + ' -c ' + srcPath + '/' + srcModel[i] + '/**/*.js');
-    // return;
+    // 更新loader可以加载jsc文件
     const thinkLoaderPath = path.join(exePath, '/node_modules/think-loader/loader/common.js');
     let thinkLoader = fs.readFileSync(thinkLoaderPath).toString();
     thinkLoader = thinkLoader.replace(/\/\\\.js\$\//g, '/\\.(js|jsc)$/');
     fs.writeFileSync(thinkLoaderPath, thinkLoader);
+    /// 更新router loader，可以加载router.jsc
+    // const thinkLoaderRouterPath = path.join(exePath, '/node_modules/think-loader/loader/router.js');
+    // thinkLoader = fs.readFileSync(thinkLoaderRouterPath).toString();
+    // if (thinkLoader.indexOf('.jsc') === -1) {
+    //   thinkLoader = thinkLoader.replace(/router\.js/g, 'router.jsc');
+    //   fs.writeFileSync(thinkLoaderRouterPath, thinkLoader);
+    // }
+
     const srcPath = path.join(exePath, '/src');
     const srcModel = fs.readdirSync(srcPath);
 
     for (let i = 0, len = srcModel.length; i < len; i++) {
-      if (srcModel[i] !== 'common1') {
+      if (srcModel[i] !== 'common11') {
         const fileList = this._findAllFile(path.join(srcPath, '/' + srcModel[i]));
         for (let j = 0, jlen = fileList.length; j < jlen; j++) {
           let content = fs.readFileSync(fileList[j]).toString();
-          //match正则：/require\(\'.*\.js\'\)/
-          let matchs = content.match(/require\(\'\..*\'\)/g);
+          // match正则：/require\(\'.*\.js\'\)/
+          const matchs = content.match(/require\(\'\..*\'\)/g);
           if (matchs) {
-            console.log(fileList[j]);
             for (let m = 0, mlen = matchs.length; m < mlen; m++) {
               const matchItem = matchs[m];
-              console.log(matchs[m]);
-              console.log(matchItem.indexOf('.js)'));
               if (matchItem.indexOf('.jsc\')') > -1) {
                 continue;
               }
@@ -46,34 +54,40 @@ module.exports = class {
             }
           }
           fs.writeFileSync(fileList[j], content);
+          if (fileList[j].indexOf('master.js') === -1 && fileList[j].indexOf('worker.js') === -1) {
+            // const obfuscationResult = JavaScriptObfuscator.obfuscate(fs.readFileSync(fileList[j]).toString());
+            // fs.writeFileSync(fileList[j], obfuscationResult.getObfuscatedCode());
+            bytenode.compileFile({
+              filename: fileList[j]
+            });
+          }
         }
-        // 编译js文件
-        console.log('开始编译js文件');
-        shell.exec('node ' + path.resolve(__dirname, '../node_modules/bytenode/cli.js').toString() + ' -c ' + srcPath + '/' + srcModel[i] + '/**/*.js');
         // // 删除js文件，只保留编译后的文件
         console.log('开始删除js文件');
         for (let j = 0, jlen = fileList.length; j < jlen; j++) {
-          // fs.unlinkSync(fileList[j]);
+          if (fileList[j].indexOf('master.js') === -1 && fileList[j].indexOf('worker.js') === -1) {
+            fs.unlinkSync(fileList[j]);
+          }
         }
       }
     }
   }
   /**
    * 遍历获取所有js文件
-   * @param {*} dir 
+   * @param {*} dir
    */
   _findAllFile(dir = '') {
-    var results = []
-    var list = fs.readdirSync(dir)
+    let results = [];
+    const list = fs.readdirSync(dir);
     list.forEach((file) => {
-      file = dir + '/' + file
-      var stat = fs.statSync(file)
+      file = dir + '/' + file;
+      var stat = fs.statSync(file);
       if (stat && stat.isDirectory()) {
         results = results.concat(this._findAllFile(file));
-      } else if (file.endsWith('.js') && file.indexOf('config.') === -1 && file.indexOf('adapter.') === -1) {
+      } else if (file.endsWith('.js') && file.indexOf('config/config.') === -1 && file.indexOf('config/adapter.') === -1 && file.indexOf('config/extend') === -1 && file.indexOf('middleware.js') === -1 && file.indexOf('validator.js') === -1 && file.indexOf('common/extend') === -1 && file.indexOf('router') === -1 && file.indexOf('model2') === -1 && file.indexOf('logic2') === -1) {
         results.push(file);
       }
-    })
-    return results
+    });
+    return results;
   }
-}
+};
